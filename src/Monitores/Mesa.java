257bca6242716c0855ab7bc4e542db2ed8218ed5
Emiliano Carrillo <emiliano.carrillo.moncayo@gmail.com>
2020-04-29 20:38:59 -0500

    Comentarios

--- src/Monitores/Mesa.java
+++ src/Monitores/Mesa.java
@@ -1,34 +1,38 @@
 package Monitores;
 
-class Mesa{
-	
+class Mesa {
+
 	ListaCircular<String> estadoOf = new ListaCircular<String>();
-	
+
 	public Mesa() {
-		for(int i =0; i < 5 ; i++) {
-			estadoOf.add("Pensando");
+		for (int i = 0; i < 5; i++) {
+			estadoOf.add("PENSAR");
 		}
-	}
-	
-	public void probar_bocado(int i) {
-		if (estadoOf.get(i) == "Hambriento" && estadoOf.get(i - 1) != "Comiendo" && estadoOf.get(i + 1) != "Comiendo") {
-			estadoOf.set(i, "Comiendo");
-		}
-	}
-	
-	public void tomar_tenedores(int i) {
-		while (estadoOf.get(i - 1) == "Comiendo" || estadoOf.get(i + 1) == "Comiendo") {
-			Util.myWait(this);
-		}
-		estadoOf.set(i, "hambriento");
-		probar_bocado(i);
 	}
 
+	public void tomar_tenedores(int i) {
+		// Condición de sincronización *********************************************
+		while (estadoOf.get(i - 1) == "COMER" || estadoOf.get(i + 1) == "COMER") {
+			// //Variable de condición *********************************************
+			Util.myWait(this);
+		}
+		estadoOf.set(i, "HAMBRE");
+		probar_bocado(i);
+	}
+	
+	// Método de ingreso ********************************
+		public void probar_bocado(int i) {
+			if (estadoOf.get(i) == "HAMBRE" && estadoOf.get(i - 1) != "COMER" && estadoOf.get(i + 1) != "COMER") {
+				estadoOf.set(i, "COMER");
+			}
+		}
+
+	// Método de ingreso ********************************
 	public void bajar_tenedores(int i) {
 		probar_bocado(i - 1);
 		probar_bocado(i + 1);
-		estadoOf.set(i, "Pensando");
-		//notify();
+		estadoOf.set(i, "PENSAR");
+		// notify();
 	}
-	
+
 }
